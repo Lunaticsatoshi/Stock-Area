74ff49341794419cf855a89c94bccfc183bacb2f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-26 17:20:56 +0530

    :zap: Updating navbar

--- client/src/components/Navbar/Navbar.vue
+++ client/src/components/Navbar/Navbar.vue
@@ -8,7 +8,10 @@
       <li>Readers</li>
     </ul>
     <div class="search">
-      <input type="text" placeholder="Search for articles, people or magazines" />
+      <input
+        type="text"
+        placeholder="Search for articles, people or magazines"
+      />
       <i class="fas fa-search"></i>
     </div>
   </div>
@@ -16,52 +19,52 @@
 
 <script>
 export default {
-    name: "Navbar"
+  name: "Navbar",
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 #navigation {
   display: flex;
   width: 100%;
   margin-bottom: 50px;
-  ul {
-    display: flex;
-    list-style: none;
-    padding: 0;
-    margin: 0 20px 0 0;
-    li {
-      font-size: 2rem;
-      padding: 2px 10px;
-      cursor: pointer;
-      &:hover {
-        color: #7ca971;
-      }
-    }
-  }
-  .search {
-    position: relative;
-    width: 100%;
-    max-width: 400px;
-    input {
-      border: none;
-      outline: none;
-      width: calc(100% - 80px);
-      padding: 15px 60px 15px 20px;
-      margin: 0;
-      border-radius: 20px;
-      background-color: #efefef;
-      font-family: "Segoe UI", Tahoma;
-      font-size: 1rem;
-    }
-    i {
-      position: absolute;
-      right: 20px;
-      top: 15px;
-      font-size: 1.6rem;
-      color: #aaa;
-      cursor: pointer;
-    }
-  }
+}
+ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0 20px 0 0;
+}
+li {
+  font-size: 2rem;
+  padding: 2px 10px;
+  cursor: pointer;
+}
+li:hover {
+  color: #7ca971;
+}
+.search {
+  position: relative;
+  width: 100%;
+  max-width: 400px;
+}
+input {
+  border: none;
+  outline: none;
+  width: calc(100% - 80px);
+  padding: 15px 60px 15px 20px;
+  margin: 0;
+  border-radius: 20px;
+  background-color: #efefef;
+  font-family: "Segoe UI", Tahoma;
+  font-size: 1rem;
+}
+.search > i {
+  position: absolute;
+  right: 20px;
+  top: 15px;
+  font-size: 1.6rem;
+  color: #aaa;
+  cursor: pointer;
 }
 </style>
