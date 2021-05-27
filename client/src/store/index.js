f86de1736fcd380b3e09b2e4cb0bd934a5727a34
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-27 17:29:07 +0530

    :package: setting up Vuex store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,23 @@
 import { createStore } from 'vuex'
+import axios from "axios"
 
 export default createStore({
   state: {
+    warehouses: []
+  },
+  getters: {
+    getWarehouses: (state) => state.warehouses,
   },
   mutations: {
+    setWarehouses(state,payload){
+      state.warehouses = payload
+    }
   },
   actions: {
+    async setWarehouses({commit}){
+      let {data} = await axios.get("http://localhost:5000/warehouses")
+      commit("setWarehouses",data)
+    }
   },
   modules: {
   }
