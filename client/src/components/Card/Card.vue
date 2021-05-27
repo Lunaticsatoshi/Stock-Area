03fb0efcf4cd95088c3946ee95e6b627f685ff80
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-27 16:49:21 +0530

    :package: Designed Card component

---
+++ client/src/components/Card/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+      <div class="card__header">
+          <h1>Card Header</h1>
+          <h5>Card Subheader</h5>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Card"
+}
+</script>
+
+<style>
+.card{
+    width: 100%;
+    margin: 10px 20px;
+    padding: 10px 10px;
+    background-color: #ffffff;
+    color: #000000;
+    height: 100px;
+    font-size: 1.5rem;
+}
+
+</style>
