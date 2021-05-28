c60f779dc256150b31582ffecbd997307052cb5e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-28 18:53:59 +0530

    :package: Filter warehouses on the basis of type

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,24 +1,45 @@
-import { createStore } from 'vuex'
-import axios from "axios"
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
-    warehouses: []
+    warehouses: [],
+    filteredWarehouses: [],
   },
   getters: {
     getWarehouses: (state) => state.warehouses,
   },
   mutations: {
-    setWarehouses(state,payload){
-      state.warehouses = payload
-    }
+    setWarehouses(state, payload) {
+      state.warehouses = payload;
+    },
+    setFilteresWarehouses(state, payload) {
+      state.filteredWarehouses = payload;
+    },
   },
   actions: {
-    async setWarehouses({commit}){
-      let {data} = await axios.get("http://localhost:5000/warehouses")
-      commit("setWarehouses",data)
-    }
+    async setWarehouses({ commit }) {
+      let { data } = await axios.get("http://localhost:5000/warehouses");
+      commit("setWarehouses", data);
+    },
+    async filterWareHouses({ commit }, filterData) {
+      let { data } = await axios.get("http://localhost:5000/warehouses");
+      switch (filterData.type) {
+        case "city":
+          data = data.filter((dataItem) => dataItem.city === filterData.value);
+          break;
+        case "cluster":
+          data = data.filter(
+            (dataItem) => dataItem.cluster === filterData.value
+          );
+          break;
+        case "spaceavailable":
+          data = data.filter(
+            (dataItem) => dataItem.spaceavailable === filterData.value
+          );
+          break;
+      }
+      commit("setFilteresWarehouses", data);
+    },
   },
-  modules: {
-  }
-})
+});
