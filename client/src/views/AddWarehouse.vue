407ba6f49eb23c7d1590f2084c7f2ecb8773113e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-29 19:04:13 +0530

    :package: Adding Warehouse view

--- client/src/views/AddWarehouse.vue
+++ client/src/views/AddWarehouse.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>
-      <h1>Add Warehouse</h1>
+  <div class="add__warehouse">
+    <h1 class="warehouse__header">Add Warehouse</h1>
   </div>
 </template>
 
 <script>
 export default {
-    name: "AddWarehouse"
-
-}
+  name: "AddWarehouse",
+};
 </script>
 
-<style>
-
+<style scoped>
+.add__warehouse {
+  width: 1300px;
+  margin: 50px auto;
+  padding: 0 10px;
+}
+.warehouse__header {
+  font-size: 3rem;
+}
+@media only screen and (max-width: 768px) {
+  .add__warehouse {
+    width: 100%;
+    margin: 50px auto;
+    padding: 0 10px;
+  }
+}
 </style>
