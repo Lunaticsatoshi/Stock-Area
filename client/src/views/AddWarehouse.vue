9aed8f9bee046611a97a62865f09e3f72c5bbf1b
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-29 19:07:31 +0530

    :package: Adding Warehouse view

--- client/src/views/AddWarehouse.vue
+++ client/src/views/AddWarehouse.vue
@@ -19,6 +19,53 @@ export default {
 .warehouse__header {
   font-size: 3rem;
 }
+.form {
+  width: 70%;
+  margin: 10px auto;
+  padding: 10px 30px;
+  background-color: #000000;
+  color: #ffffff;
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-direction: column;
+  border: 0.3px solid #ffffff;
+  border-radius: 7px;
+}
+
+.form__group {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-direction: column;
+  width: 100%;
+  height: 80px;
+  margin: 10px auto;
+}
+
+.select__group {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  margin: 10px auto;
+}
+
+.text__input {
+  height: 50px;
+  width: 100%;
+  background-color: #000000;
+  color: #ffffff;
+  border: 0.5px solid gray;
+  border-radius: 7px;
+  padding: 0 10px;
+}
+.checkbox__input {
+  margin-right: 10px;
+  width: 30px;
+  height: 15px;
+}
 @media only screen and (max-width: 768px) {
   .add__warehouse {
     width: 100%;
