6243e810deb49b7ff0bbe4e84fbd21d7d763c6d5
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-29 18:49:40 +0530

    :package: Adding add warehouse to routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -22,6 +22,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Warehouse.vue')
+  },
+  {
+    path: '/add',
+    name: 'AddWarehouse',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AddWarehouse.vue')
   }
 ]
 
