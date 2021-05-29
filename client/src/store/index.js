ac7a4e2efc27bd2e9e56cf0f7a01a7f2b4644dca
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-29 16:28:04 +0530

    :package: Set warehouse and update warehouse actions in state

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,7 +3,8 @@ import axios from "axios"
 
 export default createStore({
   state: {
-    warehouses: []
+    warehouses: [],
+    warehouse: {}
   },
   getters: {
     getWarehouses: (state) => state.warehouses,
@@ -11,12 +12,24 @@ export default createStore({
   mutations: {
     setWarehouses(state,payload){
       state.warehouses = payload
+    },
+
+    setWarehouse(state,payload){
+      state.warehouse = payload
     }
   },
   actions: {
     async setWarehouses({commit}){
       let {data} = await axios.get("http://localhost:5000/warehouses")
       commit("setWarehouses",data)
+    },
+    async setWarehouse({commit},id){
+      let {data} = await axios.get(`http://localhost:5000/warehouses/${id}`)
+      commit("setWarehouse",data)
+    },
+    async updateWarehouse({commit},warehouse){
+      let {data} = await axios.put(`http://localhost:5000/warehouses/${id}`,warehouse)
+      commit("setWarehouse",data)
     }
   },
   modules: {
