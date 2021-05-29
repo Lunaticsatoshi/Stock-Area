398ccd43b7440e6cb486170c83aa9b6a71c39fb9
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-29 16:48:37 +0530

    :package: Warehouse component in Warehouse view

--- client/src/views/Warehouse.vue
+++ client/src/views/Warehouse.vue
@@ -1,13 +1,32 @@
 <template>
-  <h1>This is a Warehouse</h1>
+  <div class="warehouse">
+    <h1>This is a Warehouse</h1>
+    <!-- <warehouse /> -->
+  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+// import Warehouse from "@/components/Warehouse";
 export default {
-
-}
+  name: "Warehouse",
+  components: {
+    // Warehouse,
+  },
+  methods: {
+    ...mapActions(["setWarehouse"]),
+  },
+  mounted() {
+    console.log(this.$route.params.id);
+    this.setWarehouse(this.$route.params.id);
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.warehouse {
+  width: 1300px;
+  margin: 50px auto;
+  padding: 0 10px;
+}
 </style>
